f482f51936f339bd1711f2dfa33bca53a47260af
Ramon Fosado <ramonft@gmail.com>
2023-12-20 14:32:06 -0600

    Docker files added

--- index.js
+++ index.js
@@ -19,18 +19,24 @@ app.get("/", function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
-
-// your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+app.get("/api", function(req, res) {
+  var now = Date.now();
+  res.json({unix: now, utc: new Date(now).toUTCString()});
 });
 
-
 app.get("/api/:date", function(req, res) {
-  var timestamp = Date.parse(req.params.date);
-  let date = timestamp ? new Date(timestamp) : 
-    new Date(parseInt(req.params.date));
-  res.json({utc: date.toUTCString(), unix: date.getTime()});
+  try {
+    var timestamp = Date.parse(req.params.date);
+    let date = timestamp ? new Date(timestamp) : 
+      new Date(parseInt(req.params.date));
+    
+    if (date == "Invalid Date")
+      throw new Error("Invalid Date");
+    
+    res.json({utc: date.toUTCString(), unix: date.getTime()});
+  } catch(error) {
+    res.json({error: error.message});
+  }
 });
 
 // listen for requests :)
